da96c6033b711d825e68e5d6fdf0917e96c9c661
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-18 14:31:49 +0100

    Fix

--- lib/doc_builder.rb
+++ lib/doc_builder.rb
@@ -100,9 +100,9 @@ class DocBuilder
   # master first, then descending numerically (by semver semantics)
   def sort_versions
     @versions.sort! do |a,b|
-      if a == 'master'
+      if a == 'main'
         -1
-      elsif b == 'master'
+      elsif b == 'main'
         1
       else
         va = a.sub(/^v/, '')
