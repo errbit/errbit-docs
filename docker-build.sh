03f6200e837bdfbfa1dd485eb66f93f647a10e7e
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-28 18:26:32 +0100

    Update

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#! /bin/sh
 
 set -eux
 
 ERRBIT_PATH=../errbit bundle exec jekyll build
 
-docker build --no-cache --pull --progress plain -t docker.io/biow0lf/errbit-docs:main .
+docker build --no-cache --pull --progress plain -t docker.io/biow0lf/errbit-docs:master .
